8f6aa47e850340bf520a3a43af559aafbb9d16aa
mehmetafsar510 <drmehmet510@gmail.com>
2021-09-03 20:25:54 +0300

    update

--- denemehttps/modules/Compute/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id" {}
-variable "public_subnets" {}
-variable "master_profile_name" {}
-variable "worker_profile_name" {}
-variable "key_name" {}
-variable "tg_port" {}
-variable "lb_target_group_arn" {}
-variable "instance_count" {}
