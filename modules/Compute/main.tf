e703d29bf28bec3a023bba3e6436064f4d5bc175
mehmetafsar510 <drmehmet510@gmail.com>
2021-09-05 00:12:11 +0300

    update

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -125,6 +125,12 @@ resource "aws_security_group" "matt-react-sg" {
   }
   ingress {
     protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol = "tcp"
     from_port = 3000
     to_port = 3000
     cidr_blocks = ["0.0.0.0/0"]
