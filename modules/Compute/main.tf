9c4384c043900588f30abbbbd61e15396228d4d7
mehmetafsar510 <drmehmet510@gmail.com>
2021-09-09 10:54:06 +0300

    update

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -3,12 +3,16 @@ data "aws_ami" "rhel8" {
   most_recent = true
   owners      = ["309956199498"]
   filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
     name   = "name"
     values = ["RHEL-8*"]
   }
 }
 resource "aws_instance" "nodejs" {
-    ami = "ami-0b0af3577fe5e3532"
+    ami = data.aws_ami.rhel8.id 
     instance_type = "t2.micro"
     iam_instance_profile = var.master_profile_name
     vpc_security_group_ids = [aws_security_group.matt-nodejs-sg.id]
@@ -23,7 +27,7 @@ resource "aws_instance" "nodejs" {
 }
 
 resource "aws_instance" "react" {
-    ami = "ami-0b0af3577fe5e3532" #data.aws_ami.rhel8.id
+    ami = data.aws_ami.rhel8.id #"ami-0b0af3577fe5e3532"
     instance_type = "t2.micro"
     iam_instance_profile = var.worker_profile_name
     vpc_security_group_ids = [aws_security_group.matt-react-sg.id]
@@ -37,7 +41,7 @@ resource "aws_instance" "react" {
 }
 
 resource "aws_instance" "postgress" {
-    ami = "ami-0b0af3577fe5e3532"
+    ami = data.aws_ami.rhel8.id 
     instance_type = "t2.micro"
     iam_instance_profile = var.worker_profile_name
     vpc_security_group_ids = [aws_security_group.matt-postgress-sg.id]
@@ -49,54 +53,6 @@ resource "aws_instance" "postgress" {
         stack = "ansible_project"
     }
 }
-
-#resource "aws_instance" "tls-ssl" {
-#    ami = data.aws_ami.rhel8.id
-#    instance_type = "t2.micro"
-#    iam_instance_profile = var.worker_profile_name
-#    vpc_security_group_ids = [aws_security_group.matt-tls-sg.id]
-#    key_name = var.key_name
-#    subnet_id = var.public_subnets
-#    tags = {
-#        Name = "ansible_tls"
-#        environment = "development"
-#        stack = "ansible_project"
-#    }
-#}
-#resource "aws_security_group" "matt-tls-sg" {
-#  name = "tls-sec-group-for-matt"
-#  vpc_id = var.vpc_id
-#  
-#  ingress {
-#    protocol = "tcp"
-#    from_port = 80
-#    to_port = 80
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  ingress {
-#    protocol = "tcp"
-#    from_port = 443
-#    to_port = 443
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  ingress {
-#    protocol = "tcp"
-#    from_port = 22
-#    to_port = 22
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  egress{
-#    protocol = "-1"
-#    from_port = 0
-#    to_port = 0
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  tags = {
-#    Name = "tls-secgroup"
-#  }
-#}
 
 resource "aws_security_group" "matt-postgress-sg" {
   name = "postgress-sec-group-for-matt"
