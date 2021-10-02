046a2f62769ced01cb76ed65cc30d6fc134907cb
mehmetafsar510 <drmehmet510@gmail.com>
2021-10-02 16:27:58 +0300

    update

--- selenium-jobs/test_owners_register_headless.py
+++ selenium-jobs/test_owners_register_headless.py
@@ -16,23 +16,41 @@ driver = webdriver.Chrome(options=chrome_options)
 url = "https://clarus.mehmetafsar.com/"
 print(url)
 driver.get(url)
-owners_link = driver.find_element_by_class_name('btn-success')
-owners_link.click()
-sleep(2)
-# Register new Owner to Todo App
+
 fn_field = driver.find_element_by_class_name('form-control')
 fn = 'Todoapp' + str(random.randint(0, 100))
 fn_field.send_keys(fn)
 driver.find_element_by_class_name('btn-success').click()
-sleep(1)
-element = driver.find_element_by_class_name('btn-warning')
-driver.execute_script("arguments[0].click();", element)
-fn_field1 = driver.find_element_by_class_name('form-control')
-fn_field1.send_keys('Clarusway')
-driver.execute_script("arguments[0].click();", element)
 sleep(2)
-fnson = 'TodoappClarusway'
 
+
+element =driver.find_element_by_xpath("/html/body/div/div/table/tbody/tr/td[3]/button")
+print("yes", element.is_displayed())
+element.click()
+sleep(2)
+
+fn_field = driver.find_element_by_class_name('form-control')
+fn = 'Todoapp'
+fn_field.send_keys(fn)
+driver.find_element_by_class_name('btn-success').click()
+sleep(2)
+
+element =driver.find_element_by_xpath("/html/body/div/div/table/tbody/tr/td[2]/button")
+print("yes", element.is_displayed())
+element.click()
+sleep(2)
+
+fn_field = driver.find_element_by_xpath("/html/body/div[1]/div/table/tbody/tr/td[2]/div/div/div/div[2]/input") 
+fn = 'Clarusway'
+fn_field.send_keys(fn)
+sleep(2)
+
+element = driver.find_element_by_xpath("/html/body/div[1]/div/table/tbody/tr/td[2]/div/div/div/div[3]/button[1]") 
+print("yes", element.is_displayed())
+element.click()
+sleep(2)
+
+fnson = 'TodoappClarusway'
 # Verify that new user is added to Owner List
 if fnson in driver.page_source:
     print(fnson, 'is added and found in the Todo')
